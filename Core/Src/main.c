3b2f9a7a2e93464362e736edd338edb93b2db6c1
angcx1997 <ang.chin.xian@gmail.com>
2022-02-08 22:24:28 +0800

    Keep unused pins in analog mode to optimize power consumption

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -51,7 +51,8 @@ osThreadId_t defaultTaskHandle;
 const osThreadAttr_t defaultTask_attributes = {
 		.name = "defaultTask",
 		.stack_size = 128 * 4,
-		.priority = (osPriority_t) osPriorityNormal, };
+		.priority = (osPriority_t) osPriorityNormal,
+};
 /* USER CODE BEGIN PV */
 
 //task handler
@@ -95,7 +96,8 @@ void rtc_timer_callback(TimerHandle_t xTimer);
  * @brief  The application entry point.
  * @retval int
  */
-int main(void) {
+int main(void)
+{
 	/* USER CODE BEGIN 1 */
 	BaseType_t status;
 	/* USER CODE END 1 */
@@ -123,7 +125,7 @@ int main(void) {
 	MX_USART3_UART_Init();
 	/* USER CODE BEGIN 2 */
 
-	HAL_UART_Receive_IT(&huart3, (uint8_t*)&user_ser_input, 1);
+	HAL_UART_Receive_IT(&huart3, (uint8_t*) &user_ser_input, 1);
 	/* USER CODE END 2 */
 
 	/* Init scheduler */
@@ -139,7 +141,7 @@ int main(void) {
 
 	/* USER CODE BEGIN RTOS_TIMERS */
 	for (int i = 0; i < 4; i++) {
-		timer_led[i] = xTimerCreate("LED Timer", pdMS_TO_TICKS(500), pdTRUE, (void*) i+1, led_effect_callback);
+		timer_led[i] = xTimerCreate("LED Timer", pdMS_TO_TICKS(500), pdTRUE, (void*) i + 1, led_effect_callback);
 		configASSERT(timer_led[i] != NULL);
 	}
 
@@ -201,7 +203,8 @@ int main(void) {
  * @brief System Clock Configuration
  * @retval None
  */
-void SystemClock_Config(void) {
+void SystemClock_Config(void)
+{
 	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
 	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
@@ -221,23 +224,27 @@ void SystemClock_Config(void) {
 	RCC_OscInitStruct.PLL.PLLN = 180;
 	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
 	RCC_OscInitStruct.PLL.PLLQ = 4;
-	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+			{
 		Error_Handler();
 	}
 	/** Activate the Over-Drive mode
 	 */
-	if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
+	if (HAL_PWREx_EnableOverDrive() != HAL_OK)
+			{
 		Error_Handler();
 	}
 	/** Initializes the CPU, AHB and APB buses clocks
 	 */
-	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
 	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
 	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
 	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
 	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+			{
 		Error_Handler();
 	}
 }
@@ -247,7 +254,8 @@ void SystemClock_Config(void) {
  * @param None
  * @retval None
  */
-static void MX_RTC_Init(void) {
+static void MX_RTC_Init(void)
+{
 
 	/* USER CODE BEGIN RTC_Init 0 */
 
@@ -268,7 +276,8 @@ static void MX_RTC_Init(void) {
 	hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
 	hrtc.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
 	hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
-	if (HAL_RTC_Init(&hrtc) != HAL_OK) {
+	if (HAL_RTC_Init(&hrtc) != HAL_OK)
+			{
 		Error_Handler();
 	}
 
@@ -283,7 +292,8 @@ static void MX_RTC_Init(void) {
 	sTime.Seconds = 0x0;
 	sTime.DayLightSaving = RTC_DAYLIGHTSAVING_NONE;
 	sTime.StoreOperation = RTC_STOREOPERATION_RESET;
-	if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BCD) != HAL_OK) {
+	if (HAL_RTC_SetTime(&hrtc, &sTime, RTC_FORMAT_BCD) != HAL_OK)
+			{
 		Error_Handler();
 	}
 	sDate.WeekDay = RTC_WEEKDAY_MONDAY;
@@ -291,7 +301,8 @@ static void MX_RTC_Init(void) {
 	sDate.Date = 0x1;
 	sDate.Year = 0x0;
 
-	if (HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BCD) != HAL_OK) {
+	if (HAL_RTC_SetDate(&hrtc, &sDate, RTC_FORMAT_BCD) != HAL_OK)
+			{
 		Error_Handler();
 	}
 	/* USER CODE BEGIN RTC_Init 2 */
@@ -305,7 +316,8 @@ static void MX_RTC_Init(void) {
  * @param None
  * @retval None
  */
-static void MX_USART3_UART_Init(void) {
+static void MX_USART3_UART_Init(void)
+{
 
 	/* USER CODE BEGIN USART3_Init 0 */
 
@@ -322,7 +334,8 @@ static void MX_USART3_UART_Init(void) {
 	huart3.Init.Mode = UART_MODE_TX_RX;
 	huart3.Init.HwFlowCtl = UART_HWCONTROL_NONE;
 	huart3.Init.OverSampling = UART_OVERSAMPLING_16;
-	if (HAL_UART_Init(&huart3) != HAL_OK) {
+	if (HAL_UART_Init(&huart3) != HAL_OK)
+			{
 		Error_Handler();
 	}
 	/* USER CODE BEGIN USART3_Init 2 */
@@ -334,7 +347,8 @@ static void MX_USART3_UART_Init(void) {
 /**
  * Enable DMA controller clock
  */
-static void MX_DMA_Init(void) {
+static void MX_DMA_Init(void)
+{
 
 	/* DMA controller clock enable */
 	__HAL_RCC_DMA1_CLK_ENABLE();
@@ -354,16 +368,19 @@ static void MX_DMA_Init(void) {
  * @param None
  * @retval None
  */
-static void MX_GPIO_Init(void) {
+static void MX_GPIO_Init(void)
+{
 	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
 
 	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOE_CLK_ENABLE();
 	__HAL_RCC_GPIOC_CLK_ENABLE();
+	__HAL_RCC_GPIOF_CLK_ENABLE();
 	__HAL_RCC_GPIOH_CLK_ENABLE();
 	__HAL_RCC_GPIOA_CLK_ENABLE();
 	__HAL_RCC_GPIOB_CLK_ENABLE();
-	__HAL_RCC_GPIOD_CLK_ENABLE();
 	__HAL_RCC_GPIOG_CLK_ENABLE();
+	__HAL_RCC_GPIOD_CLK_ENABLE();
 
 	/*Configure GPIO pin Output Level */
 	HAL_GPIO_WritePin(GPIOB, LD1_Pin | LD3_Pin | LD2_Pin, GPIO_PIN_RESET);
@@ -371,11 +388,45 @@ static void MX_GPIO_Init(void) {
 	/*Configure GPIO pin Output Level */
 	HAL_GPIO_WritePin(USB_PowerSwitchOn_GPIO_Port, USB_PowerSwitchOn_Pin, GPIO_PIN_RESET);
 
+	/*Configure GPIO pins : PE2 PE3 PE4 PE5
+	 PE6 PE7 PE8 PE9
+	 PE10 PE11 PE12 PE13
+	 PE14 PE15 PE0 PE1 */
+	GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5
+			| GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9
+			| GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13
+			| GPIO_PIN_14 | GPIO_PIN_15 | GPIO_PIN_0 | GPIO_PIN_1;
+	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+
 	/*Configure GPIO pin : USER_Btn_Pin */
 	GPIO_InitStruct.Pin = USER_Btn_Pin;
 	GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
 	GPIO_InitStruct.Pull = GPIO_NOPULL;
 	HAL_GPIO_Init(USER_Btn_GPIO_Port, &GPIO_InitStruct);
+
+	/*Configure GPIO pins : PF0 PF1 PF2 PF3
+	 PF4 PF5 PF6 PF7
+	 PF8 PF9 PF10 PF11
+	 PF12 PF13 PF14 PF15 */
+	GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3
+			| GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7
+			| GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11
+			| GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
+	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
+
+	/*Configure GPIO pins : PC0 PC2 PC3 PC6
+	 PC7 PC8 PC9 PC10
+	 PC11 PC12 */
+	GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_6
+			| GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9 | GPIO_PIN_10
+			| GPIO_PIN_11 | GPIO_PIN_12;
+	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
 	/*Configure GPIO pins : RMII_MDC_Pin RMII_RXD0_Pin RMII_RXD1_Pin */
 	GPIO_InitStruct.Pin = RMII_MDC_Pin | RMII_RXD0_Pin | RMII_RXD1_Pin;
@@ -384,6 +435,14 @@ static void MX_GPIO_Init(void) {
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
 	GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
 	HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+	/*Configure GPIO pins : PA0 PA3 PA4 PA5
+	 PA6 PA15 */
+	GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5
+			| GPIO_PIN_6 | GPIO_PIN_15;
+	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 	/*Configure GPIO pins : RMII_REF_CLK_Pin RMII_MDIO_Pin RMII_CRS_DV_Pin */
 	GPIO_InitStruct.Pin = RMII_REF_CLK_Pin | RMII_MDIO_Pin | RMII_CRS_DV_Pin;
@@ -400,6 +459,26 @@ static void MX_GPIO_Init(void) {
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
 	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
+	/*Configure GPIO pins : PB1 PB2 PB10 PB11
+	 PB12 PB15 PB3 PB4
+	 PB5 PB6 PB8 PB9 */
+	GPIO_InitStruct.Pin = GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_10 | GPIO_PIN_11
+			| GPIO_PIN_12 | GPIO_PIN_15 | GPIO_PIN_3 | GPIO_PIN_4
+			| GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_8 | GPIO_PIN_9;
+	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+	/*Configure GPIO pins : PG0 PG1 PG2 PG3
+	 PG4 PG5 PG8 PG9
+	 PG10 PG12 PG14 PG15 */
+	GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3
+			| GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_8 | GPIO_PIN_9
+			| GPIO_PIN_10 | GPIO_PIN_12 | GPIO_PIN_14 | GPIO_PIN_15;
+	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
 	/*Configure GPIO pin : RMII_TXD1_Pin */
 	GPIO_InitStruct.Pin = RMII_TXD1_Pin;
 	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
@@ -407,6 +486,18 @@ static void MX_GPIO_Init(void) {
 	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
 	GPIO_InitStruct.Alternate = GPIO_AF11_ETH;
 	HAL_GPIO_Init(RMII_TXD1_GPIO_Port, &GPIO_InitStruct);
+
+	/*Configure GPIO pins : PD10 PD11 PD12 PD13
+	 PD14 PD15 PD0 PD1
+	 PD2 PD3 PD4 PD5
+	 PD6 PD7 */
+	GPIO_InitStruct.Pin = GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13
+			| GPIO_PIN_14 | GPIO_PIN_15 | GPIO_PIN_0 | GPIO_PIN_1
+			| GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5
+			| GPIO_PIN_6 | GPIO_PIN_7;
+	GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
 	/*Configure GPIO pin : USB_PowerSwitchOn_Pin */
 	GPIO_InitStruct.Pin = USB_PowerSwitchOn_Pin;
@@ -466,7 +557,7 @@ void led_effect_callback(TimerHandle_t xTimer) {
 
 }
 
-void rtc_timer_callback(TimerHandle_t xTimer){
+void rtc_timer_callback(TimerHandle_t xTimer) {
 	rtc_show_time_date_itm();
 }
 
@@ -474,18 +565,18 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
 	uint8_t dummy;
 
-	if(!xQueueIsQueueFullFromISR(queue_data))
-	{
+	if (!xQueueIsQueueFullFromISR(queue_data))
+			{
 		//Enqueue data type
-		xQueueSendFromISR(queue_data, (void*)&user_ser_input,NULL);
+		xQueueSendFromISR(queue_data, (void* )&user_ser_input, NULL);
 	}
 	else
 	{
-		if(user_ser_input == '\n')
-		{
+		if (user_ser_input == '\n')
+				{
 			//Make sure that last data byte of the queue is '\n'
-			xQueueReceiveFromISR(queue_data, (void*)&dummy, NULL);
-			xQueueSendFromISR(queue_data, (void*)&user_ser_input, NULL);
+			xQueueReceiveFromISR(queue_data, (void*) &dummy, NULL);
+			xQueueSendFromISR(queue_data, (void* )&user_ser_input, NULL);
 		}
 	}
 
@@ -493,7 +584,7 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 	if (user_ser_input == '\n')
 		xTaskNotifyFromISR(task_command, 0, eNoAction, NULL);
 
-	HAL_UART_Receive_IT(&huart3, (uint8_t*)&user_ser_input,	1);
+	HAL_UART_Receive_IT(&huart3, (uint8_t*) &user_ser_input, 1);
 }
 /* USER CODE END 4 */
 
@@ -504,7 +595,8 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
  * @retval None
  */
 /* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void *argument) {
+void StartDefaultTask(void *argument)
+{
 	/* USER CODE BEGIN 5 */
 	/* Infinite loop */
 	for (;;) {
@@ -521,7 +613,8 @@ void StartDefaultTask(void *argument) {
  * @param  htim : TIM handle
  * @retval None
  */
-void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
 	/* USER CODE BEGIN Callback 0 */
 
 	/* USER CODE END Callback 0 */
@@ -537,7 +630,8 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
  * @brief  This function is executed in case of error occurrence.
  * @retval None
  */
-void Error_Handler(void) {
+void Error_Handler(void)
+{
 	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
